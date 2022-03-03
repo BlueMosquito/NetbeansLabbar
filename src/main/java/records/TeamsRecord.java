33a87c44b1c67a15bac784ed8e15f0867576ceff
BlueMosquito <hnajafi7171@gmail.com>
2022-03-03 18:57:46 +0100

    Secund Commit Lab3

---
+++ src/main/java/records/TeamsRecord.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package records;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.BelongsTo;
+import org.javalite.activejdbc.annotations.IdName;
+import org.javalite.activejdbc.annotations.Table;
+
+/**
+ *
+ * @author hamednajafi
+ */
+@Table("teams")
+@IdName("id")
+@BelongsTo(parent = GamesRecord.class, foreignKeyName = "g_id")
+public class TeamsRecord extends Model{
+    
+}
